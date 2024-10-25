65289c162ec392d877db7725f5b501b982f9e546
Tobse95 <scto1041@hs-karlsruhe.de>
2024-10-25 14:24:14 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<!DOCTYPE html>
-<html lang="de">
+  template: `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -61,8 +60,7 @@ import { Component } from '@angular/core';
 <footer>
     <p>&copy; 2024 PLSDIN - All Rights Reserved</p>
 </footer>
-</body>
-</html>`,
+</body>`,
 })
 export class AppComponent {
   value = 'World';
